9ddd561b16e0905f32024338aa21793fa377bfd9
Daniel Mendler <mail@daniel-mendler.de>
2016-12-30 14:31:08 +0100

    add lens compat test

---
+++ test/LensCompatibility.hs
@@ -0,0 +1,8 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module LensCompatibility (
+  module Intro,
+  module Control.Lens
+) where
+
+import Intro
+import Control.Lens
---
+++ test/lens.hs
@@ -0,0 +1,7 @@
+{-# LANGUAGE NoImplicitPrelude #-}
+module Main where
+
+import LensCompatibility
+
+main :: IO ()
+main = putStrLn "Intro is compatible with Control.Lens"
