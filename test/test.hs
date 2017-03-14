f9c9c13b297c4ba453af2e82692053742290e78c
Daniel Mendler <mail@daniel-mendler.de>
2017-03-14 02:52:15 +0100

    add shortbytestring support

--- test/test.hs
+++ test/test.hs
@@ -5,8 +5,9 @@
 {-# LANGUAGE ScopedTypeVariables #-}
 module Main where
 
-import Intro
 import BaseCompat ()
+import Data.ByteString.Short (ShortByteString)
+import Intro
 import LensCompat ()
 import Test.QuickCheck
 import Test.QuickCheck.Instances ()
@@ -15,18 +16,23 @@ main :: IO ()
 main = do
   encode (Proxy :: Proxy LText)       (Proxy :: Proxy ByteString)
   encode (Proxy :: Proxy LText)       (Proxy :: Proxy LByteString)
+  encode (Proxy :: Proxy LText)       (Proxy :: Proxy ShortByteString)
   encode (Proxy :: Proxy LText)       (Proxy :: Proxy [Word8])
   encode (Proxy :: Proxy String)      (Proxy :: Proxy ByteString)
   encode (Proxy :: Proxy String)      (Proxy :: Proxy LByteString)
+  encode (Proxy :: Proxy String)      (Proxy :: Proxy ShortByteString)
   encode (Proxy :: Proxy String)      (Proxy :: Proxy [Word8])
   encode (Proxy :: Proxy Text)        (Proxy :: Proxy ByteString)
   encode (Proxy :: Proxy Text)        (Proxy :: Proxy LByteString)
+  encode (Proxy :: Proxy Text)        (Proxy :: Proxy ShortByteString)
   encode (Proxy :: Proxy Text)        (Proxy :: Proxy [Word8])
   iso    (Proxy :: Proxy ByteString)  (Proxy :: Proxy ByteString)
   iso    (Proxy :: Proxy ByteString)  (Proxy :: Proxy LByteString)
+  iso    (Proxy :: Proxy ByteString)  (Proxy :: Proxy ShortByteString)
   iso    (Proxy :: Proxy ByteString)  (Proxy :: Proxy [Word8])
   iso    (Proxy :: Proxy LByteString) (Proxy :: Proxy ByteString)
   iso    (Proxy :: Proxy LByteString) (Proxy :: Proxy LByteString)
+  iso    (Proxy :: Proxy LByteString) (Proxy :: Proxy ShortByteString)
   iso    (Proxy :: Proxy LByteString) (Proxy :: Proxy [Word8])
   iso    (Proxy :: Proxy LText)       (Proxy :: Proxy LText)
   iso    (Proxy :: Proxy LText)       (Proxy :: Proxy String)
@@ -39,6 +45,7 @@ main = do
   iso    (Proxy :: Proxy Text)        (Proxy :: Proxy Text)
   iso    (Proxy :: Proxy [Word8])     (Proxy :: Proxy ByteString)
   iso    (Proxy :: Proxy [Word8])     (Proxy :: Proxy LByteString)
+  iso    (Proxy :: Proxy [Word8])     (Proxy :: Proxy ShortByteString)
   iso    (Proxy :: Proxy [Word8])     (Proxy :: Proxy [Word8])
 
 iso :: forall a b proxy. (Eq a, Show a, Arbitrary a, ConvertString a b, ConvertString b a) => proxy a -> proxy b -> IO ()
