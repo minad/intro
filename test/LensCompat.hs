908081564edce420a67fffee4fcb682259f9f835
Daniel Mendler <mail@daniel-mendler.de>
2017-04-05 10:35:17 +0200

    disable lens test for ghc 8.2

--- test/LensCompat.hs
+++ test/LensCompat.hs
@@ -1,7 +1,11 @@
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE CPP #-}
 module LensCompat (
   module X
 ) where
 
 import Intro as X
+
+#if __GLASGOW_HASKELL__ < 820
 import Control.Lens as X
+#endif
