401244f90ead7a0ea90db782b84898a42008010d
Daniel Mendler <mail@daniel-mendler.de>
2017-06-09 22:13:59 +0200

    test lens

--- test/LensCompat.hs
+++ test/LensCompat.hs
@@ -1,11 +1,7 @@
 {-# LANGUAGE NoImplicitPrelude #-}
-{-# LANGUAGE CPP #-}
 module LensCompat (
   module X
 ) where
 
 import Intro as X
-
-#if __GLASGOW_HASKELL__ < 820
 import Control.Lens as X
-#endif
